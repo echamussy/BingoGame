cb2247323d16b10f2e0964e6f6be1c2c7f8f5a44
Enrique Rodriguez <echamussy@gmail.com>
2018-12-17 15:34:04 +0100

    Much better layout now with better constraints in the cell view

--- BingoGame/Classes/UI/BingoCardCollectionViewCell.swift
+++ BingoGame/Classes/UI/BingoCardCollectionViewCell.swift
@@ -9,30 +9,14 @@ import UIKit
 
 class BingoCardCollectionViewCell: UICollectionViewCell {
 
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var cardContainerView: UIView!
-    @IBOutlet weak var containerWidthConstraint: NSLayoutConstraint!
-    @IBOutlet weak var containerHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
     }
     
-    
-    
     public func render(card:BingoCard){
-        /*
-        let cellSize = self.frame.size
-        if (cellSize.width < cellSize.height){
-            self.containerWidthConstraint.constant = cellSize.height
-            self.containerHeightConstraint.constant = cellSize.height
-        } else {
-            self.containerWidthConstraint.constant = cellSize.width
-            self.containerHeightConstraint.constant = cellSize.width
-        }*/
-        
-        self.nameLabel.text = card.name
-        
+        self.imageView.image = UIImage(named: "bingo-closed-card")
     }
 
 }
